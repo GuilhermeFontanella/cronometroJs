ef499cc163a382efdb7246af5124957d20fe207b
Guilherme Fontanella <gui.jmf@hotmail.com>
2021-03-28 15:07:06 -0300

    relogio com todas as funcoes funcionando

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ var hora = 00;
 var minuto = 00;
 var segundo = 0;
 var milesimos = 0;
+var intervalo;
+var ligado = false;
 
 function inicioRelogio() {
     milesimos++
+    ligado = true;
     document.getElementById("milesimos").innerHTML = milesimos;
-    
     if(milesimos >= 99) {
         milesimos = 00;
         segundo++;
@@ -25,16 +27,35 @@ function inicioRelogio() {
     if(minuto >= 59) {
         minuto = 00;
         hora++;
-        document.getElementById("horas").innerHTML = minuto;
+        document.getElementById("horas").innerHTML = horas;
     }   
 };
 
 function start() {
-    setInterval(inicioRelogio, 10);
+    if(!ligado) {
+        this.intervalo = setInterval(inicioRelogio, 10);
+    }
 }
 
 function stop() {
+    this.ligado = false;
+    clearInterval(intervalo);
+    return intervalo = null; 
+    
+    
 };
 
-function zero() {
+function zero() { 
+    hora = 0;
+    minuto = 0;
+    segundo = 0;
+    milesimos = 0;
+    document.getElementById("horas").innerHTML = "00";
+    document.getElementById("minutos").innerHTML = "00";
+    document.getElementById("segundos").innerHTML = "00";
+    document.getElementById("milesimos").innerHTML = "00";
 };
+
+
+
+
